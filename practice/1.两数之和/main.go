88e12fd5f30426fda5bfc81f32ef445a79c0b336
ZGGSONG <zggsong@foxmail.com>
2022-08-01 18:50:31 +0800

    perf: 优化第一题

--- practice/1.两数之和/main.go
+++ practice/1.两数之和/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"log"
-)
+import "fmt"
 
 /*
 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
@@ -14,10 +12,23 @@ func main() {
 	var nums []int = []int{2, 7, 11, 15}
 	var target = 9
 	res := twoSum(nums, target)
-	log.Println(res)
+	fmt.Println(res)
 }
 
 func twoSum(nums []int, target int) []int {
+	tmp := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		if _, ok := tmp[another]; ok {
+			return []int{tmp[another], i}
+		}
+		tmp[nums[i]] = i
+	}
+	return nil
+}
+
+//xx
+func twoSumOld(nums []int, target int) []int {
 	var res []int
 	for k, v := range nums {
 		for i, j := range nums {
