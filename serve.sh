24c2616081d8820f6368ac3990dab48af3043132
ZGGSONG <zggsong@foxmail.com>
2022-08-04 15:20:32 +0800

    add_shell

---
+++ serve.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+msg=""
+if [ "${1}" == "" ]; then
+  msg="update"
+else
+  msg="${1}"
+fi
+git add .
+
+git commit -m "${msg}"
+
+echo "commit with msg: ${msg}"
+
+git push origin main
+
+echo "push to server"
+
+git push github main
+
+echo "push to github"
