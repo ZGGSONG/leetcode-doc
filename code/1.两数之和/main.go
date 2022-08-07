1c269b036ebbc1810b2f8605313990f3b6568f2a
ZGGSONG <zggsong@foxmail.com>
2022-08-07 18:21:29 +0800

    1

--- code/1.两数之和/main.go
+++ code/1.两数之和/main.go
@@ -2,41 +2,21 @@ package main
 
 import "fmt"
 
-/*
-给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target的那两个整数，并返回它们的数组下标。
-你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
-你可以按任意顺序返回答案。
-
-*/
 func main() {
-	var nums []int = []int{2, 7, 11, 15}
-	var target = 9
+	nums := []int{3, 2, 4}
+	target := 6
 	res := twoSum(nums, target)
 	fmt.Println(res)
 }
 
+//开辟一个新空间存已经遍历过的值，后面的再去与前面的值进行比较，是否满足和为target，否则继续存，而因为map的时间复杂度为O(1)，所以比使用数组进行查询更好O(n)
 func twoSum(nums []int, target int) []int {
-	tmp := make(map[int]int)
+	ret := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		another := target - nums[i]
-		if _, ok := tmp[another]; ok {
-			return []int{tmp[another], i}
+		if key, ok := ret[target-nums[i]]; ok {
+			return []int{key, i}
 		}
-		tmp[nums[i]] = i
+		ret[nums[i]] = i
 	}
 	return nil
-}
-
-//xx
-func twoSumOld(nums []int, target int) []int {
-	var res []int
-	for k, v := range nums {
-		for i, j := range nums {
-			if v+j == target && i != k {
-				res = append(res, i)
-				res = append(res, k)
-			}
-		}
-	}
-	return res
 }
