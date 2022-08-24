e6d522ac80f9aeab04b1f3d7c55822858d07b03b
ZGGSONG <zggsong@foxmail.com>
2022-08-24 14:38:28 +0800

    update

--- code/27.移除元素/main.go
+++ code/27.移除元素/main.go
@@ -1,30 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	nums := []int{1, 3, 3, 4}
+	nums := []int{1, 2, 3, 3, 3, 4, 5, 6}
 	val := 3
-	element := removeElement(nums, val)
-	fmt.Println(element)
+	res := removeElement(nums, val)
+	fmt.Printf("removeElement: %v\n", res)
 }
 
-//输入：nums = [3,2,2,3], val = 3
-//输出：2, nums = [2,2]
-//注意：nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案
 func removeElement(nums []int, val int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-	j := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != val {
-			if i != j {
-				nums[i], nums[j] = nums[j], nums[i]
-			}
-			//标记为当前=val的下标，也即所求数组长度
-			j++
+	var slowIndex int
+	for fastIndex := 0; fastIndex < len(nums); fastIndex++ {
+		if nums[fastIndex] != val {
+			nums[slowIndex] = nums[fastIndex]
+			slowIndex++
 		}
 	}
-	return j
+	return slowIndex
 }
---
+++ code/27.移除元素/main_bak.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 3, 3, 4}
+	val := 3
+	element := removeElement(nums, val)
+	fmt.Println(element)
+}
+
+//输入：nums = [3,2,2,3], val = 3
+//输出：2, nums = [2,2]
+//注意：nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案
+func removeElement1(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			if i != j {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+			//标记为当前=val的下标，也即所求数组长度
+			j++
+		}
+	}
+	return j
+}
