458179f99ba8be666019a998c1ab6e620827ec29
ZGGSONG <zggsong@foxmail.com>
2022-10-06 22:28:55 +0800

    feat: feat new test template

--- template.sh
+++ template.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# func name
+echo -e "\033[42;37m---------------\033[0m"
+echo -e "\033[42;37m填入方法名(moveZeroes)\033[0m"
+echo -e "\033[42;37m---------------\033[0m"
+read FUNC
+if [[ -z $FUNC ]]; then
+  FUNC="default"
+fi
+echo -e "\033[42;37m当前方法名: ${FUNC} \033[0m"
+
+# 中文名
+echo -e "\033[42;37m---------------\033[0m"
+echo -e "\033[42;37m填入中文名(121. 买卖股票的最佳时机)\033[0m"
+echo -e "\033[42;37m---------------\033[0m"
+read INDEX CNNAME
+if [[ -z $CNNAME || -z $INDEX ]]; then
+  INDEX="0."
+  CNNAME="默认题目"
+fi
+echo -e "\033[42;37m当前文件名: ${INDEX} ${CNNAME} \033[0m"
+
+
 # 分组
 echo -e "\033[42;37m---------------\033[0m"
 echo -e "\033[42;37m填入分组名(array)\033[0m"
@@ -10,39 +32,43 @@ if [[ -z $FILEPATH ]]; then
 fi
 echo -e "\033[42;37m当前分组: ${FILEPATH} \033[0m"
 
+if [ ! -d "./docs/${FILEPATH}" ]; then
+  mkdir ./docs/${FILEPATH}
+fi
+
 # 英文名
 echo -e "\033[42;37m---------------\033[0m"
-echo -e "\033[42;37m填入题目名(0118.pascals-triangle)\033[0m"
+echo -e "\033[42;37m填入文件名(0118.pascals-triangle)\033[0m"
 echo -e "\033[42;37m---------------\033[0m"
 read FILENAME
 if [[ -z $FILENAME ]]; then
   FILENAME="0000.default"
 fi
-echo -e "\033[42;37m当前题目名: ${FILENAME} \033[0m"
+echo -e "\033[42;37m当前文件名: ${FILENAME} \033[0m"
 
-# 中文名
-#echo -e "\033[42;37m---------------\033[0m"
-#echo -e "\033[42;37m填入中文名(121. 买卖股票的最佳时机)\033[0m"
-#echo -e "\033[42;37m---------------\033[0m"
-#read INDEX CNNAME
-#if [[ -z $CNNAME || -z $INDEX ]]; then
-#  INDEX="0."
-#  CNNAME="默认题目"
-#fi
-#echo -e "\033[42;37m当前文件名: ${INDEX} ${CNNAME} \033[0m"
+# test func
+testFunc=$(echo ${FUNC} | awk '{for (i=1;i<=NF;i++)printf toupper(substr($i,0,1))substr($i,2,length($i))" ";printf "\n"}')
 
-if [ ! -d "./docs/${FILEPATH}" ]; then
-  mkdir ./docs/${FILEPATH}
-fi
-
+# 建立英文名code
 if [ ! -d "./code/${FILENAME}" ]; then
   mkdir ./code/${FILENAME}
-  echo "package main
+  echo "package ${FUNC}
 
-func main() {
+func ${FUNC}() {
 
 }
-" > ./code/${FILENAME}/${FILENAME}.go
+" > ./code/${FILENAME}/${FUNC}.go
+# add *.test.go
+  echo "package ${FUNC}
+
+import (
+  \"testing\"
+)
+
+func Test${testFunc}(t *testing.T) {
+
+}
+" > ./code/${FILENAME}/${FUNC}_test.go
 fi
 
 
@@ -90,8 +116,8 @@ LeetCode: [https://leetcode.cn/problems/${ENTITLE}/](https://leetcode.cn/problem
 
 ## 解题思路
 
-- "> ./docs/${FILEPATH}/${FILENAME}+".md"
+- "> ./docs/${FILEPATH}/${FILENAME}.md
 
-echo "Successfully created docs/${FILEPATH}/${FILENAME}"
+echo "Successfully created docs/${FILEPATH}/${FILENAME}.md"
 
 exit 0
